d0a0d538303f961172ef4a97aafb12ac4ea9fa5d
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-10 12:41:55 +0100

    added recent lections in js file

---
+++ main.js
@@ -0,0 +1,34 @@
+//ES 5
+var name = "anikken";
+
+//ES6
+let seconName = "Tom";
+
+secondName = "Max"; // change let variable value using its name,
+
+const thirdName = "Marius The great";
+
+thirdName = "Julie"; // change const variable
+
+// 2. console.log -- console.dir console.info // yellow // console.error
+
+// 3. >= <= = ==
+
+"H" = "H"
+// its equal and true, we are using not strict equality
+
+"H" === "h" // false is called strict equality. it means same type, same structure etc
+
+"1" = 1 // true
+"1" === 1 // false, one is string and one is numeric so not exactly the same
+
+// switch case
+
+// 5 objects
+// Whatever is inside the curly brackets is objects. objects contains items. key and values.
+//example
+
+let car = {
+    color: "red" // color is key and red is value. together they are the object of variable car
+}
+
